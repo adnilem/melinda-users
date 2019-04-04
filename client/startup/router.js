58b3113a23d2d42533297f12325bb1a2a9c85a03
Melinda <femminis.mda@gmail.com>
2019-04-04 12:30:20 +0200

    Route pour le body

--- client/startup/router.js
+++ client/startup/router.js
@@ -3,8 +3,17 @@ import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 //Info dans la doc FlowRouter classique et extra 
+//Route test
 FlowRouter.route('/test', {
     action() {
         console.log('Route test !');
+    }
+});
+
+//Remplacer le body par une route !
+//Création d'une route a la racine qui affiche le templace navbar
+FlowRouter.route('/', {
+    action() {
+        BlazeLayout.render('navbar');
     }
 });
